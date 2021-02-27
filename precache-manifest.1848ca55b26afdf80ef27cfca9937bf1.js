aea19e922a82790dee71fb8a4b8d7f4cd7762d48
Staffan Strömsholm <52479640+StaffanStromsholm@users.noreply.github.com>
2021-02-27 22:53:13 +0200

    Updates

---
+++ precache-manifest.1848ca55b26afdf80ef27cfca9937bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab406bb1e81eaf511437e5a721671a51",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "56eb533782da6104d2a3",
+    "url": "/covid19tracker/static/css/main.4e4f4155.chunk.css"
+  },
+  {
+    "revision": "ccf0829397e2220de5c0",
+    "url": "/covid19tracker/static/js/2.f65919e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19tracker/static/js/2.f65919e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56eb533782da6104d2a3",
+    "url": "/covid19tracker/static/js/main.1288d564.chunk.js"
+  },
+  {
+    "revision": "b21437349aeda0dbdfab",
+    "url": "/covid19tracker/static/js/runtime-main.c66ccc54.js"
+  }
+]);
